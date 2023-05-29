f484638ad6452250bb991125b519bc20220df9d1
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-05-29 11:29:36 -0300

    Criado Projeto de Gerenciamento

---
+++ app/src/main/java/NewApp/NewClass.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NewApp;
+
+import controller.ProjectController;
+import controller.TaskController;
+import java.util.Date;
+import java.util.List;
+import model.Project;
+import model.Task;
+
+/**
+ *
+ * @author SimView
+ */
+public class NewClass {
+    
+    public static void main(String[] args) {
+        
+ //      ProjectController projectController = new ProjectController();
+        
+ //       Project project = new Project();
+ //       project.setName("Projeto Teste");
+ //       project.setDescription("Descrição");
+ //       projectController.save(project);
+        
+  //      project.setId(2);
+  //      project.setName("Mudando o nome");
+ //       project.setDescription("Esta descrição também será alterada");
+ //       projectController.update(project);
+        
+ //      List<Project> projects = projectController.getAll();
+ //      System.out.println("Total de projetos = " + projects.size());
+ 
+ //       projectController.removeById(3);
+ 
+        TaskController taskController = new TaskController();
+        Task task = new Task();
+//        task.setName("Segunda tarefa salva");
+//        task.setDescription("Uma nova descrição");
+//        task.setNotes("Sem notas");
+//        task.setIsCompleted(false);
+//        task.setIdProject(1);
+//        task.setDeadline(new Date());
+        
+//        taskController.save(task);
+
+//        task.setId(1);
+//       task.setIdProject(2);
+//        task.setName("Mudando o nome");
+//        task.setDescription("A descrição também foi alterada");
+//        task.setNotes("Agora temos uma nota");
+//        taskController.update(task);
+        
+//        List<Task> tasks = taskController.getAll(2);
+//        System.out.println("Total de tarefas: " + tasks.size());
+        
+        
+    }
+    
+}
