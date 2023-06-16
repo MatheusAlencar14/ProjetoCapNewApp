618d75ecaf56c87d725d15f15b294dd1467f00d4
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-06-16 15:05:08 -0300

    Customizando a renderização da coluna de Prazo

--- app/src/main/java/view/MainScreen.java
+++ app/src/main/java/view/MainScreen.java
@@ -14,6 +14,7 @@ import java.util.List;
 import javax.swing.DefaultListModel;
 import model.Project;
 import model.Task;
+import util.DeadlineColumnCellRederer;
 import util.TaskTableModel;
 
 /**
@@ -30,10 +31,11 @@ public class MainScreen extends javax.swing.JFrame {
     
     public MainScreen() {
         initComponents();
-        decorateTableTask();
         
         initDataController();
         initComponentsModel();
+        
+        decorateTableTask();
     }
 
     /**
@@ -459,8 +461,10 @@ public class MainScreen extends javax.swing.JFrame {
         jTableTasks.getTableHeader().setBackground(new Color(0, 153, 153));
         jTableTasks.getTableHeader().setForeground(new Color(255, 255, 255));
         
+        jTableTasks.getColumnModel().getColumn(2).setCellRenderer(new DeadlineColumnCellRederer());
+        
         //Criando um sort automático para as colunas da tabela
-        jTableTasks.setAutoCreateRowSorter(true);
+        //jTableTasks.setAutoCreateRowSorter(true);
          
     }
     
